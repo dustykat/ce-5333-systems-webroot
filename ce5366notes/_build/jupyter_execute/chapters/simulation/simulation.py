541528acdc19000db7a4f4d57807bdf60ed02d19
theodore cleveland <ted.cleveland@gmail.com>
2023-01-19 14:44:49 -0600

    add chapoter

--- ce5366notes/_build/jupyter_execute/chapters/simulation/simulation.py
+++ ce5366notes/_build/jupyter_execute/chapters/simulation/simulation.py
@@ -1,4 +1,82 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Simulation Models for Decision Support
+# # Modeling for Decision Support
+# 
+# Design and management decisions that are made about environmental and water resources systems, are based on what the decision-makers believe will take place as a result of their decisions. 
+# These predictions are either based on very qualitative informa-
+# tion or, at least in part, on quantitative information provided by an analog (mouse model for drug toxicity; column study for pollutant transport; ...), and/or mathematical (computer-based) models. 
+# These quantitative mathematical models are considered essential for carrying out environmental impact assessments. 
+# Mathematical simulation and optimization models packaged within interactive computer programs provide a common way
+# for planners and managers to predict the behaviour of any proposed water resources system design or management policy before it is implemented
+# 
+# Modelling provides the principal way of predicting the behaviour of proposed infrastructural designs or management policies. The past thirty years have witnessed major advances in our abilities to model the engineering, economic, ecological, hydrological and sometimes even the institutional or political impacts of large, complex, multipurpose water resources systems. Appli-
+# cations of models to real systems improves understanding, and contributes to improved system design, management and operation.
+# 
+# Problems motivating modelling and analyses exhibit a number of common characteristics:
+# 
+# >A systems focus or orientation. In such situations atten-
+# tion needs to be devoted to the interdependencies and
+# interactions of elements within the system as a whole,
+# as well as to the elements themselves.
+# 
+# >The use of interdisciplinary teams. In many complex and non-traditional problems it is not at all clear from the start what disciplinary viewpoints will turn out to be most appropriate or acceptable. It is essential that participants in such work – coming from different established disciplines – become familiar with the
+# techniques, vocabulary and concepts of the other
+# disciplines involved. Participation in interdisciplinary
+# modelling often requires a willingness to make mistakes at the fringes of one’s technical competence and
+# to accept less than the latest advances in one’s own
+# discipline.
+# 
+# >The use of formal mathematics. Most analysts prefer
+# to use mathematical models to assist in system
+# description and the identification and evaluation of
+# efficient tradeoffs among conflicting objectives, and to
+# provide an unambiguous record of the assumptions
+# and data used in the analysis.
+# 
+# Not all water resources planning and management problems are suitable candidates for study using modelling
+# methods. Modelling is most appropriate when:
+# 
+# >The planning and management objectives are reasonably well defined and organizations and individuals can be identified who can benefit from understanding the model results.
+# 
+# >There are many alternative decisions that may satisfy the stated objectives, and the best decision is not obvious.
+# 
+# >The water resources system and the objectives being analysed are describable by reasonably tractable mathematical representations.
+# 
+# >The information needed, such as the hydrological, economic, environmental and ecological impacts resulting from any decision, can be better estimated through the use of models.
+# 
+# > The parameters of these models are estimable from readily obtainable data.
+# 
+# ## Model Building Approach
+# 
+# The modeling approach distilled into a protocol (vaguely reminiscent of the scientific method) is summarized as:
+# 
+# 1. Identify the information the model is to provide. This includes criteria or measures of system performance that are of interest to stakeholders. These criteria or measures are defined as functions of the behaviour or state of the system being modelled. 
+# 2. Model the behaviour so the state of the system associated with any ‘external’ inputs can be predicted. This requires modelling the physical, chemical, biological, economic, ecological and social processes that take place, as applicable, in the represented system. 
+# 3. Integrate these two parts along with some means of entering the ‘external’ inputs and obtaining in meaningful ways the outputs.
+# 4. Calibrate and verify or validate. 
+# 5. Apply the model to produce the information desired.
+# 
+# There is a fair amount of effort in the steps above, and a lot of art. But even using software for a specific situation involves these steps in some sense.
+# 
+# ## Simulation Models
+# 
+# Simulation models address "what if" questions: What will likely happen over time and at one or more specific places if a particular design and/or operating policy is implemented?
+# 
+# Simulation works well when there are only a relatively few alternatives to be evaluated, not when there are a large number of them. The trial and error process of simulation is time consuming. An important role of optimization methods is to reduce the number of alternatives for simulation analyses. However, if only one method of analysis is to be used to evaluate a complex water resources system, simulation together with human judgement concerning which alternatives to simulate is often, and rightly so, the method of choice.
+# 
+# ## Useful Model Types:
+# 
+# ### Hydraulic Models
+# 
+# ### Quality Models
+# 
+# ### Cost Models
+# 
+# ### Integrated Models
+
+# In[ ]:
+
+
+
+
